a2d714b50c559153a976861f84e271056a0ff082
Atul-me <atulkur1999@gmail.com>
2024-06-04 23:07:29 +0530

    private added

--- client/src/context/auth.js
+++ client/src/context/auth.js
@@ -1,12 +1,16 @@
 import { useState,  useContext, createContext, useEffect } from "react";
-
+import axios from "axios";
 const AuthContext = createContext();
 
 const AuthProvider = ({children}) =>{
     const [auth, setAuth] = useState({
         user:null,
         token:"",
-    })
+    });
+
+    //default axios
+    axios.defaults.headers.common['Authorization'] = auth?.token
+    
     useEffect(() => {
         const data = localStorage.getItem("auth");
         if(data){
@@ -17,8 +21,8 @@ const AuthProvider = ({children}) =>{
                 token:parseData.token,
             });
         };
-
-    },[auth])
+        //eslint-disable-next-line
+    },[])
     return (
         <AuthContext.Provider value={[auth,setAuth]}>
             {children}
