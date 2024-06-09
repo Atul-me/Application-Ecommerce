22e2150c5b2dcc4af2101b7884b1297d0e8a354b
Atul-me <atulkur1999@gmail.com>
2024-06-09 19:06:12 +0530

    client side

--- client/src/pages/Admin/CreateCategory.js
+++ client/src/pages/Admin/CreateCategory.js
@@ -1,25 +1,68 @@
-import React from 'react'
-import AdminMenu from '../../components/Layout/AdminMenu'
-import Layout from '../../components/Layout/Layout'
+import React, { useEffect, useState } from "react";
+import AdminMenu from "../../components/Layout/AdminMenu";
+import Layout from "../../components/Layout/Layout";
+import toast from "react-hot-toast";
+import axios from "axios";
 
 const CreateCategory = () => {
+  const [categories, setCategories] = useState([]);
+
+  //get all category
+  const getAllCategory = async () => {
+    try {
+      const { data } = await axios.get("/api/v1/category/get-category");
+      if (data.success) {
+        setCategories(data.category);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong in getting category");
+    }
+  };
+
+  useEffect(() => {
+    getAllCategory();
+  }, []);
   return (
     <Layout title="DNS-Shop Category">
-        <div className="container mx-auto p-4">
+      <div className="container mx-auto p-4">
         <div className="grid grid-cols-12 gap-4">
           <div className="col-span-3">
-            <AdminMenu/>
+            <AdminMenu />
           </div>
           <div className="col-span-9">
             <div className="bg-white p-6 rounded-lg shadow-md">
-              <h1 className='text-center'>All Categories</h1>
+              <h1 className="text-center">Manage Category : 5:31</h1>
+              <div className="">
+                <div className="relative overflow-x-auto">
+                  <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                      <tr>
+                        <th scope="col" className="px-6 py-3">
+                          Name
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                          Actions
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                        {categories.map((c) => (
+                          <td key={c._id}>{c.name}</td>
+                        ))}
+                      </tr>
+                      
+                    </tbody>
+                  </table>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    
     </Layout>
-  )
-}
+  );
+};
 
-export default CreateCategory
+export default CreateCategory;
